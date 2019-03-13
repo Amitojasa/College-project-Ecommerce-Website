521953b2ae4819a598769feb29ff9a9cbe8c7474
Amitojasa <amitojvmc@gmail.com>
2019-03-13 23:53:06 +0530

    backup of sql file

---
+++ gadgetspick.sql
@@ -0,0 +1,212 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 13, 2019 at 07:21 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `gadgetspick`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logintb`
+--
+
+CREATE TABLE IF NOT EXISTS `logintb` (
+`id` int(11) NOT NULL,
+  `emailId` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `linkUserId` int(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `logintb`
+--
+
+INSERT INTO `logintb` (`id`, `emailId`, `password`, `linkUserId`) VALUES
+(1, 'amitoj@dgfd.cpm', '1234', 8),
+(2, 'as@ahuja.com', '1234', 9),
+(3, 'as@ahuja.com', '1234', 10),
+(4, 'amitoj@sdf.com', '1234', 11),
+(5, 'amitojsingh1990@gmail.com', '1234', 12),
+(6, 'amitojsingh1990@gmail.com', '1234', 13),
+(7, 'amitojvmc@gmail.com', '1234', 14),
+(8, 'amitojvmc@gmail.com', '1234', 15),
+(9, 'amitojvmc@gmail.com', '1234', 16),
+(10, 'amitojvmc@gmail.com', '1234', 17);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newsletter`
+--
+
+CREATE TABLE IF NOT EXISTS `newsletter` (
+`id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `newsletter`
+--
+
+INSERT INTO `newsletter` (`id`, `email`) VALUES
+(1, 'amitoj@dgfd.cpm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statekeys`
+--
+
+CREATE TABLE IF NOT EXISTS `statekeys` (
+  `abbr` varchar(255) NOT NULL,
+  `statename` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `statekeys`
+--
+
+INSERT INTO `statekeys` (`abbr`, `statename`) VALUES
+('AP', 'Andhra Pradesh'),
+('AR', 'Arunachal Pradesh'),
+('AS', 'Assam'),
+('BR', 'Bihar'),
+('CT', 'Chhattisgarh'),
+('GA', 'Goa'),
+('GJ', 'Gujarat'),
+('HR', 'Haryana'),
+('HP', 'Himachal Pradesh'),
+('JK', 'Jammu and Kashmir'),
+('JH', 'Jharkhand'),
+('KA', 'Karnataka'),
+('KL', 'Kerala'),
+('MP', 'Madhya Pradesh'),
+('MH', 'Maharashtra'),
+('MN', 'Manipur'),
+('ML', 'Meghalaya'),
+('MZ', 'Mizoram'),
+('NL', 'Nagaland'),
+('OR', 'Orissa'),
+('PB', 'Punjab'),
+('RJ', 'Rajasthan'),
+('SK', 'Sikkim'),
+('TN', 'Tamil Nadu'),
+('TS', 'Telangana'),
+('TR', 'Tripura'),
+('UK', 'Uttarakhand'),
+('UP', 'Uttar Pradesh'),
+('WB', 'West Bengal'),
+('AN', 'Andaman and Nicobar Islands'),
+('CH', 'Chandigarh'),
+('DN', 'Dadar and Nagar Haveli'),
+('DD', 'Daman and Diu'),
+('DL', 'Delhi'),
+('LD', 'Lakshadeep'),
+('PY', 'Pondicherry (Puducherry)'),
+('', 'Select a State');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userdetailstb`
+--
+
+CREATE TABLE IF NOT EXISTS `userdetailstb` (
+`id` int(11) NOT NULL,
+  `firstName` varchar(100) NOT NULL,
+  `lastName` varchar(100) NOT NULL,
+  `emailAddress` varchar(100) NOT NULL,
+  `contactNo1` varchar(15) NOT NULL,
+  `contactNo2` varchar(15) NOT NULL,
+  `shipaddr1` varchar(200) NOT NULL,
+  `shipaddr2` varchar(200) NOT NULL,
+  `shiplandmark` varchar(200) NOT NULL,
+  `country` varchar(10) NOT NULL DEFAULT 'India',
+  `state` varchar(200) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `postalcode` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userdetailstb`
+--
+
+INSERT INTO `userdetailstb` (`id`, `firstName`, `lastName`, `emailAddress`, `contactNo1`, `contactNo2`, `shipaddr1`, `shipaddr2`, `shiplandmark`, `country`, `state`, `city`, `postalcode`) VALUES
+(1, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(2, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(3, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(4, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(5, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(6, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(7, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(8, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(9, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(10, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(11, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(12, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(13, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(14, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4-A', 'power house road', 'adjacent to overseas bank', 'India', 'Andhra Pradesh', 'Bathinda', '151001'),
+(15, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(16, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001'),
+(17, 'Amitoj', 'singh', 'amitojvmc@gmail.com', '9417171800', '9417102100', '21500, 6/4', 'power house road', 'adjacent to overseas bank', 'India', 'Punjab', 'Bathinda', '151001');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `logintb`
+--
+ALTER TABLE `logintb`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `newsletter`
+--
+ALTER TABLE `newsletter`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userdetailstb`
+--
+ALTER TABLE `userdetailstb`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `logintb`
+--
+ALTER TABLE `logintb`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `newsletter`
+--
+ALTER TABLE `newsletter`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `userdetailstb`
+--
+ALTER TABLE `userdetailstb`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
