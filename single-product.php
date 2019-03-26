ca25381eb434ced3493bbb82fa4f880c910211a3
Amitojasa <amitojvmc@gmail.com>
2019-03-27 00:04:33 +0530

    work under progress

--- single-product.php
+++ single-product.php
@@ -1,24 +1,42 @@
 <?php include 'header.php'; ?>
+<?php include 'conn.inc.php'; ?>
+<?php 
+    $cat=@$_GET['category'];
+    $id=@$_GET['id'];
+?>
 <link rel="stylesheet" href="css/single-product.css">
 <div class="container-fluid my-3">
     <div class="row">
         <div class="left col-sm-5">
             <div class="slideshow">
                 <div class="card">
+                
+                <?php 
+                 $selectQuery="select * from productdetails where category = '$cat' and id=$id";
+
+                    $qu = mysqli_query($conn,$selectQuery) or die(mysqli_error($conn));
+                    $q=mysqli_fetch_assoc($qu);
+                ?>
                     <div class="card-body p-3 m-2">
                     <div id="demo" class="demo carousel slide" data-ride="carousel">
 
                         <!-- The slideshow -->
                         <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="images/test.jpeg" alt="Los Angeles" width="100%" max-height="400px">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="images/test2.jpeg" alt="Los Angeles" width="100%" max-height="400px">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="images/test.jpeg" alt="Los Angeles" width="100%" max-height="400px">
-                            </div>
+                            <?php if(!empty($q['image1'])){ ?>
+                                <div class="main-imgs carousel-item active">
+                                    <img class="img-fluid align-self-center" src="<?php echo "images/".$q['category']."/".$q['image1'];?>" alt="Los Angeles" >
+                                </div>
+                            <?php } ?>
+                            <?php if(!empty($q['image2'])){ ?>
+                                <div class="img main-imgs carousel-item">
+                                    <img class="img-fluid align-self-center" src="<?php echo "images/".$q['category']."/".$q['image2'];?>" alt="Los Angeles">
+                                </div>
+                            <?php } ?>
+                            <?php if(!empty($q['image3'])){ ?>
+                                <div class="img main-imgs carousel-item">
+                                    <img class="img-fluid align-self-center" src="<?php echo "images/".$q['category']."/".$q['image3'];?>" alt="Los Angeles" >
+                                </div>
+                            <?php } ?>
                         </div>
 
                         <!-- Left and right controls -->
@@ -30,13 +48,21 @@
                         </a>
                          <!-- Indicators -->
                          <ul class="carousel-indicators">
-                            <li data-target="#demo" data-slide-to="0" class="active"><img src="images/test.jpeg" alt="Los Angeles" width="100px" height="100px"></li>
-                            <li data-target="#demo" data-slide-to="1">
-                            <img src="images/test2.jpeg" alt="Los Angeles" width="100px" height="100px">
-                            </li>
-                            <li data-target="#demo" data-slide-to="2">
-                            <img src="images/test.jpeg" alt="Los Angeles" width="100px" height="100px">
-                            </li>
+                            <?php if(!empty($q['image1'])){ ?>
+                                <li data-target="#demo" data-slide-to="0" class="active">
+                                    <img src="<?php echo "images/".$q['category']."/".$q['image1'];?>" alt="Los Angeles" width="100px" height="100px">
+                                </li>
+                            <?php } ?>
+                            <?php if(!empty($q['image2'])){ ?>
+                                <li data-target="#demo" data-slide-to="1">
+                                    <img src="<?php echo "images/".$q['category']."/".$q['image2'];?>" alt="Los Angeles" width="100px" height="100px">
+                                </li>
+                            <?php } ?>
+                            <?php if(!empty($q['image3'])){ ?>
+                                <li data-target="#demo" data-slide-to="2">
+                                    <img src="<?php echo "images/".$q['category']."/".$q['image3'];?>" alt="Los Angeles" width="100px" height="100px">
+                                </li>
+                            <?php } ?>
                         </ul>
                         </div>
                     </div>
@@ -52,53 +78,48 @@
             <div class="card border-0">
                 <div class="card-body">
                     <div class="title">
-                        <h5>Lenovo Ideapad 320 Core i3 6th Gen - (4 GB/1 TB HDD/DOS) IP 320-15ISK Laptop  (15.6 inch, Onyx Black, 2.2 kg)</h5>
+                        <h5><?php echo $q['title']; ?></h5>
                     </div>
                     <div class="rating text-secondary my-2 bold">
                             <span class="badge badge-primary">4.4 <li class="fa fa-star"></li></span> 100 Ratings | 107 reviews
                     </div>
                     <div class="price">
-                            <h6 class="text-dark"><span class="text-secondary">Our-Price: </span>&#8377; 45500</h6>
-                            <h6  class="text-secondary">MRP: <strike class="text-danger"><span  class="text-dark"> &#8377; 49500</span></strike></h6>
-                            <h6 class="text-dark"><span class="text-secondary">You Save: </span>&#8377; 4000</h6>
+                            <h6 class="text-dark"><span class="text-secondary">Our-Price: </span>&#8377; <?php echo $q['newPrice']; ?></h6>
+                            <h6  class="text-secondary">MRP: <strike class="text-danger"><span  class="text-dark"> &#8377; <?php echo $q['oldPrice']; ?></span></strike></h6>
+                            <h6 class="text-dark"><span class="text-secondary">You Save: </span>&#8377; <?php echo $q['oldPrice']-$q['newPrice']; ?></h6>
                             
                     </div>
                     <div class="stock">
-                        <h5 class="text-success">In stock</h5>
+                        <?php if($q['stock']>0){?>
+                            <h5 class="text-success"> <?php echo "In Stock"; ?></h5>
+                        <?php }else{?>
+                            <h5 class="text-danger"> <?php echo "Out Of Stock"; ?></h5>
+                        <?php } ?>
+
                         <form>
                             <div class="input-group-inline">
                             <b><label for="sel1">Quantity:</label></b>
-                            <select class="form-control" id="sel1">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
+                            <?php $quant=$q['stock']%4;?>
+                            <select class="form-control" id="sel1" <?php if($q['stock']==0){echo "disabled";}?> >
+                                <?php 
+                                if($q['stock']>0){
+                                for($i=0;$i<=$quant;$i++){?>
+                                <option><?php echo $i+1; ?></option>
+                                <?php }} ?>
                             </select>
                         </form>
                     </div>
                     <br>
                     <div class="product-details">
                         <h5>Product Details</h5>
-                        <ul>
-                            <li>
-                                Processor: 7th Generation Intel core i3-7020U processor, 2.3GHz base processor speed
-                            </li>
-                            <li>
-                                Operating System: Pre-loaded Windows 10 Home with lifetime validity
-                            </li>
-                            <li>
-                                Display: 14-inch HD (1366x768) display
-                            </li>
-                            <li>
-                                Memory &amp; Storage: 4GB DDR4 |Storage:1TB HDD
-                            </li>
-                            <li>
-                                Design &amp; battery: Thin and light design|Laptop weight: 1.59 kg|Average battery life = 4 hours, Lithium battery
-                            </li>
-                            <li>
-                                Warranty:This genuine HP laptop comes with 1 year domestic warranty from HP covering manufacturing defects and not covering physical damage. For more details, see Warranty section below.
-                            </li>	
-                            <a href="">See more product details</a>			
+                        <?php $features=(explode(";",$q['description'])); ?>
+                            <ul>
+                            <?php foreach($features as $i){
+                                if($i!="")
+                                echo "<li>$i</li>";
+                            }
+                            ?>
+                            <a href="#details">See more product details</a>			
                         </ul>
                         
                     </div>
@@ -164,56 +185,25 @@
     </div>
 </section>
 
-<section class="details row">
+<section class="details row" id="details">
+                            <?php $features=(explode(";",$q['details']));
+                                $detailsArray = array();
+                                foreach($features as $i){
+
+                                    $f=(explode(":",$i));
+                                    
+                                    $detailsArray[$f[0]] = @$f[1];
+                                }
+                            ?>
     <div class="col-md-6">
     <div class="secHeader mb-3">
            <h5>Product Details</h5>
     </div>
 <table class="table" cellspacing="0" cellpadding="0" border="0">
 <tbody>
-    <tr><td class="bg-light px-2">Brand</td><td class="value px-2">HP</td></tr>
-    <tr><td class="bg-light px-2">Series</td><td class="value px-2">HP 14</td></tr>
-    <tr><td class="bg-light px-2">Colour</td><td class="value px-2">Smoke Gray</td></tr>
-    <tr><td class="bg-light px-2">Item Height</td><td class="value px-2">20 Millimeters</td></tr>
-    <tr><td class="bg-light px-2">Item Width</td><td class="value px-2">23.4 Centimeters</td></tr>
-    <tr><td class="bg-light px-2">Screen Size</td><td class="value px-2">14 Inches</td></tr>
-    <tr><td class="bg-light px-2">Maximum Display Resolution</td><td class="value px-2">1366x768</td></tr>
-    <tr class="size-weight"><td class="bg-light px-2">Item Weight</td><td class="value px-2">1.59 Kg</td></tr>
-    <tr class="size-weight"><td class="bg-light px-2">Product Dimensions</td><td class="value px-2">33.5 x 23.4 x 2 cm</td></tr>
-    <tr class="batteries"><td class="bg-light px-2">Batteries:</td><td class="value px-2">1 Lithium ion batteries required. (included)</td></tr>
-    <tr class="item-model-number"><td class="bg-light px-2">Item model number</td><td class="value px-2">HP 14q-cs0006TU</td></tr>
-    <tr><td class="bg-light px-2">Processor Brand</td><td class="value px-2">Intel</td></tr>
-             <tr><td class="bg-light px-2">Processor Type</td><td class="value px-2">Core i3</td></tr>
-         <tr><td class="bg-light px-2">Processor Speed</td><td class="value px-2">2.3 GHz</td></tr>
-         <tr><td class="bg-light px-2">RAM Size</td><td class="value px-2">4 GB</td></tr>
-         <tr><td class="bg-light px-2">Memory Technology</td><td class="value px-2">DDR4-2133 SDRAM</td></tr>
-         <tr><td class="bg-light px-2">Computer Memory Type</td><td class="value px-2">GDDR4</td></tr>
-         <tr><td class="bg-light px-2">Hard Drive Size</td><td class="value px-2">1 TB</td></tr>
-         <tr><td class="bg-light px-2">Hard Disk Technology</td><td class="value px-2">Mechanical Hard Drive</td></tr>
-         <tr><td class="bg-light px-2">Hard Drive Interface</td><td class="value px-2">eSATA</td></tr>
-         <tr><td class="bg-light px-2">Audio Details</td><td class="value px-2">Headphones</td></tr>
-         <tr><td class="bg-light px-2">Speaker Description</td><td class="value px-2">Dual Speakers</td></tr>
-         <tr><td class="bg-light px-2">Graphics Coprocessor</td><td class="value px-2">Intel HD Graphics</td></tr>
-         <tr><td class="bg-light px-2">Connectivity Type</td><td class="value px-2">Intel 802.11bgn (1x1) Wi-Fi and Bluetooth 4.2 Combo</td></tr>
-         <tr><td class="bg-light px-2">Wireless Type</td><td class="value px-2">802.11bgn</td></tr>
-         <tr><td class="bg-light px-2">Number of USB 2.0 Ports</td><td class="value px-2">1</td></tr>
-         <tr><td class="bg-light px-2">Number of USB 3.0 Ports</td><td class="value px-2">2</td></tr>
-         <tr><td class="bg-light px-2">Number of HDMI Ports</td><td class="value px-2">1</td></tr>
-         <tr><td class="bg-light px-2">Number of Audio-out Ports</td><td class="value px-2">1</td></tr>
-         <tr><td class="bg-light px-2">Number of Ethernet Ports</td><td class="value px-2">1</td></tr>
-         <tr><td class="bg-light px-2">Number of Microphone Ports</td><td class="value px-2">1</td></tr>
-         <tr><td class="bg-light px-2">Voltage</td><td class="value px-2">220</td></tr>
-         <tr><td class="bg-light px-2">Wattage</td><td class="value px-2">41 Watts</td></tr>
-         <tr><td class="bg-light px-2">Hardware Platform</td><td class="value px-2">PC</td></tr>
-         <tr><td class="bg-light px-2">Operating System</td><td class="value px-2">Windows 10 Home</td></tr>
-         <tr><td class="bg-light px-2">Average Battery Life (in hours)</td><td class="value px-2">4 Hours</td></tr>
-         <tr><td class="bg-light px-2">Lithium Battery Energy Content</td><td class="value px-2">41 Watt Hours</td></tr>
-         <tr><td class="bg-light px-2">Lithium battery Weight</td><td class="value px-2">100 Grams</td></tr>
-         <tr><td class="bg-light px-2">Number of Lithium Ion Cells</td><td class="value px-2">3</td></tr>
-         <tr><td class="bg-light px-2">Included Components</td><td class="value px-2">Laptop, Battery and AC Adapter</td></tr>
-         <tr><td class="bg-light px-2">Date First Available</td><td class="value px-2">9 August 2018</td></tr>
-<tr><td class="lAttr">&nbsp;</td><td class="lAttr">&nbsp;</td></tr>
-
+    <?php foreach($detailsArray as $i=>$j){?>
+    <tr><td class="bg-light px-2"><?php echo $i;?></td><td class="value px-2"><?php echo $j;?></td></tr>
+    <?php } ?>
      </tbody>
      </table>
 
@@ -257,7 +247,7 @@
             <h5>Warranty &amp; Support</h5>
             </div>
             <div class="text-justify">
-                <strong>Warranty Details:</strong> 1 year onsite domestic warranty from HP covering manufacturing defects and not covering physical damage. Register on brand website to activate warranty.For FAQs on warranty, link to check and update warranty start date, see Warranty FAQ PDF under Technical Specifications. Reach HP customer care at the 1800 200 0047(toll free), between 9am - 9pm. This product is eligible for 10 Days Replacement in case of any product damage, defects or different product being shipped.
+                <strong>Warranty Details:</strong> <?php echo $q['warranty'];?>
             </div>
         </div>
         <div class="Reviews">
