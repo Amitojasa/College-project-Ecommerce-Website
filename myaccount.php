4c0d3306e6abc7ec4665cbd66c4123fe57320424
Amitojasa <amitojvmc@gmail.com>
2019-04-06 13:51:42 +0530

    work under progress

--- myaccount.php
+++ myaccount.php
@@ -80,11 +80,16 @@
 
 <?php include 'header.php'?>
 
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 <script>
 function favor(id){
-        $.ajax({
+    $.ajax({
             url:"favo.php?id="+id, //the page containing php script
             type: "POST", //request type
             success:function(result){
@@ -278,7 +283,7 @@ function removeFromCart(id){
                         </div>
                     </div>
                 </div>
-                <div class="row px-4 py-2" id="tab">
+                <div class="row py-2" id="tab">
                     <div class="col-12 mb-xs-30">
                     <div class="cart-item-table commun-table">
                         <div class="table-responsive">
@@ -477,7 +482,7 @@ function removeFromCart(id){
     var type = window.location.hash.substr(1);
     if(type=="") type="dashboard";
     // document.getElementById('#'+type).classList.add("active");
-    $("a[href$='#"+type+"']").addClass("active");
+    $("a[href$='#"+type+"']").tab('show');
 </script>
 <?php include 'footer.php'?>
 
