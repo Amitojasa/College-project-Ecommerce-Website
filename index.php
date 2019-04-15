9e7c4e8839a9e20ef34edbccfc70e9abb62cfe52
Amitojasa <amitojvmc@gmail.com>
2019-04-16 00:16:46 +0530

    work under progress

--- index.php
+++ index.php
@@ -52,17 +52,25 @@ include 'header.php';
                     $q=mysqli_fetch_assoc($qu);
                 ?>
                     <div class="card">
-                        <img class="card-img-top" src="<?php echo "images/".$q['category']."/".$q['image1'];?>" alt="Card image">
+                        <div class="card-img">
+                            <img class="card-img-top" src="<?php echo "images/".$q['category']."/".$q['image1'];?>" alt="Card image">
+                        </div>
                         <div class="card-body pt-2">
-                            <h6 class="card-title my-1"><?php echo $q['title'];?></h6>
-                            <p class="card-text p-0 my-1"><?php echo $q['category'];?></p>
-                            <h5 class="card-title p-0 m-0 mb-2"> &#8377; <?php echo $q['newPrice'];?> <strike class="text-danger"><small class="text-secondary"> &#8377; <?php echo $q['oldPrice'];?></small></strike> </h5>
+                            <div class="card-title-outer">
+                                <p class="card-title my-1" title="<?php echo $q['title'];?>"><?php echo $q['title'];?>
+                                </p>
+                                <span style="background-color:white;color:#49b0c1;position:absolute;bottom:0;right:0;padding-left:10px;">...</span>
+                                
+                            </div>
+                            <h6 class="card-text p-0 my-1"><?php echo $q['category'];?></h6>
+                            <h5 class="card-price p-0 m-0 mb-2"> &#8377; <?php echo $q['newPrice'];?> <strike class="text-danger"><small class="text-secondary"> &#8377; <?php echo $q['oldPrice'];?></small></strike> </h5>
                             <a href="#" class="btn btn-primary px-5">View</a>
                         </div>
                     </div>
                 <?php } ?>
                 </div>
             </div>
+            <?php if(count($l)>5){?>
             <div class="carousel-item">
                 <div class="card-deck">
                 <?php
@@ -83,6 +91,7 @@ include 'header.php';
                 <?php } ?>
                 </div>
             </div>
+                <?php }?>
         </div>
         <a class="carousel-control-prev" href="#demo1" data-slide="prev">
             <span class="carousel-control-prev-icon"></span>
@@ -117,7 +126,7 @@ include 'header.php';
                         <div class="card-body pt-2">
                             <h5 class="card-title my-1">Lenovo Ideapad </h5>
                             <p class="card-text p-0 my-1">Lenovo Ideapad 330 Core</p>
-                            <h5 class="card-title p-0 m-0 mb-2"> &#8377; 49500 <strike class="text-danger"><small class="text-secondary"> &#8377; 60000</small></strike> </h5>
+                            <h5 class="card-price p-0 m-0 mb-2"> &#8377; 49500 <strike class="text-danger"><small class="text-secondary"> &#8377; 60000</small></strike> </h5>
                             <a href="#" class="btn btn-primary px-5">View</a>
                         </div>
                     </div>
@@ -126,7 +135,7 @@ include 'header.php';
                         <div class="card-body pt-2">
                             <h5 class="card-title my-1">Lenovo Ideapad </h5>
                             <p class="card-text p-0 my-1">Lenovo Ideapad 330 Core</p>
-                            <h5 class="card-title p-0 m-0 mb-2"> &#8377; 49500 <strike class="text-danger"><small class="text-secondary"> &#8377; 60000</small></strike> </h5>
+                            <h5 class="card-price p-0 m-0 mb-2"> &#8377; 49500 <strike class="text-danger"><small class="text-secondary"> &#8377; 60000</small></strike> </h5>
                             <a href="#" class="btn btn-primary px-5">View</a>
                         </div>
                     </div>
